a170048678cf7ca44b78e65bc663d451f9b3d536
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-04-16 22:06:24 +0700

    feat: add feature data product

--- LARAVEL/tokobuku/routes/web.php
+++ LARAVEL/tokobuku/routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('stisla', function () {
-    return view('stisla');
-});
+Route::get('product', [ProductController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
